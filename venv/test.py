f5b35a2fce1586c29bd506db88eca70a63de8e8e
zarth <98376279+za-rth@users.noreply.github.com>
2024-06-29 20:38:15 +0800

    Im a new branch BEECH

---
+++ venv/test.py
@@ -0,0 +1,19 @@
+import numpy as np;
+
+def main():
+
+
+
+    x= input("prompt first number: ")
+    y = input("prompt second number: ");
+
+    sum = int(x) + int(y)
+
+    print("Total sum is: " , sum)
+
+    np.linspace
+
+    return 0;
+
+if __name__ =="__main__":
+    main();
